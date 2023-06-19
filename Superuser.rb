defb1fb931290222d07f55247d93073051f2e710
pairnon <janpairnon@gmail.com>
2023-06-19 17:31:48 +1000

    add constructor & accessor

--- Superuser.rb
+++ Superuser.rb
@@ -1,3 +1,15 @@
 class Superuser < User
 
+    def initialize(name, id, password)
+        super(name, id)
+        @logged_in = false
+        # TODO password hashing
+    end
+
+    def get_log_in_status()
+        return @logged_in
+    end
+
+    # TODO
+
 end
